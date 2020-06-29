dd31fff37a9b03c8ff9afca9f29e7f38a82ef53e
Karan Singh <karansingh1559@gmail.com>
2020-06-29 22:26:55 +0530

    cleaned admin handlers

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,17 @@
+//MODULE IMPORTS
 const express = require('express');
-const router = express.Router();
-
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
-const { validateSignup, Admin, validateLogin } = require('../models/admin');
+const { Admin } = require('../models/admin');
 
+//ROUTER OBJECT INIT
+const router = express.Router();
+
+//ADMIN LOGIN {POST}
 router.post('/login', async (req, res) => {
     try {
-        const {error} = validateLogin(req.body);
-        if (error) {
-            return res.status(400).send("Invalid Inputs");
-        }
+        // Validate Email
         const admin = await Admin.findOne({
             email: req.body.email
         });
@@ -19,53 +19,61 @@ router.post('/login', async (req, res) => {
             return res.status(404).send("Invalid Email!");
         }
 
+        // Validate Password
         const passwordVerify = await bcrypt.compare(req.body.password, admin.password);
         if (!passwordVerify) {
             return res.status(404).send("Incorrect Password!");
         }
 
+        //JWT Authentication
         const token = jwt.sign({
             _id: admin._id,
             isAdmin: admin.isAdmin
         }, "jwtPrivateKey");
 
-        return res.header('x-auth-header', token).status(200).send({message : "login successful check the header"});
+        //Send the token in the response header
+        return res.header('x-auth-header', token)
+            .status(200)
+            .send("Login Successful!");
     } catch (err) {
         console.log(err);
     }
 });
 
+//ADMIN SIGNUP {POST}
 router.post('/signup', async (req, res) => {
     try {
-        const {error} = validateSignup(req.body);
-        if (error) {
-            return res.status(400).send("Invalid Inputs");
-        }
-
+        // // Check if Admin already exists
         let admin = await Admin.findOne({
             email: req.body.email
         });
         if (admin) {
-            return res.status(400).send("Admin Already Exists");
+            return res.status(400).send("Admin Already Exists!");
         }
 
+        //Create a new Admin instance
         admin = new Admin(req.body);
 
+        //Modify it to store a hashed password
         const salt = await bcrypt.genSalt(10);
         admin.password = await bcrypt.hash(admin.password, salt);
         
+        //Save the admin to the Atlas Cluster
         const data = await admin.save();
+
+        //Generate a JSON Web Token
         const token = jwt.sign({
             _id: data._id,
             isAdmin: data.isAdmin
         }, "jwtPrivateKey");
 
+        //Send the generated token back as a response
         return res.status(200).json({
-            message: "Signup Successful",
+            message: "SignUp Successful!",
             token
         });
     } catch (err) {
-        return res.status(400).send("ERROR: Unsuccessful Signup!");
+        return res.send(err)
     }
 });
 
